39ddb95bf4152ad1a38781b7c069abbc67c2451c
Julien Malik <julien.malik@paraiso.me>
2018-03-21 09:25:42 +0100

    add test_bitshuffle_roundtrip_neon, compute elapsed time

--- tests/test_bitshuffle_roundtrip_neon.c
+++ tests/test_bitshuffle_roundtrip_neon.c
@@ -11,13 +11,13 @@
 
 #include "test_common.h"
 #include "../blosc/shuffle.h"
-#include "../blosc/shuffle-generic.h"
+#include "../blosc/bitshuffle-generic.h"
 
 
 /* Include NEON-accelerated shuffle implementation if supported by this compiler.
    TODO: Need to also do run-time CPU feature support here. */
 #if defined(SHUFFLE_NEON_ENABLED)
-  #include "../blosc/shuffle-neon.h"
+  #include "../blosc/bitshuffle-neon.h"
 #else
   #if defined(_MSC_VER)
     #pragma message("NEON shuffle tests not enabled.")
@@ -28,8 +28,10 @@
 
 
 /** Roundtrip tests for the NEON-accelerated shuffle/unshuffle. */
-static int test_shuffle_roundtrip_neon(size_t type_size, size_t num_elements,
+static int test_bitshuffle_roundtrip_neon(size_t type_size, size_t num_elements,
                                        size_t buffer_alignment, int test_type) {
+
+fprintf(stdout, "type_size=%d num_elements=%d buffer_alignment=%d test_type=%d\n", type_size, num_elements, buffer_alignment, test_type);
 #if defined(SHUFFLE_NEON_ENABLED)
   size_t buffer_size = type_size * num_elements;
 
@@ -38,32 +40,35 @@ static int test_shuffle_roundtrip_neon(size_t type_size, size_t num_elements,
   void* shuffled = blosc_test_malloc(buffer_alignment, buffer_size);
   void* unshuffled = blosc_test_malloc(buffer_alignment, buffer_size);
 
+  void* tmp_buf = blosc_test_malloc(buffer_alignment, buffer_size);
+
   /* Fill the input data buffer with random values. */
   blosc_test_fill_random(original, buffer_size);
 
   /* Shuffle/unshuffle, selecting the implementations based on the test type. */
   blosc_timestamp_t start;
   blosc_set_timestamp(&start);
+
   switch (test_type) {
     case 0:
       /* neon/neon */
-      shuffle_neon(type_size, buffer_size, original, shuffled);
-      unshuffle_neon(type_size, buffer_size, shuffled, unshuffled);
+      bitshuffle_neon(original, shuffled, num_elements, type_size, tmp_buf);
+      bitunshuffle_neon(shuffled, unshuffled, num_elements, type_size, tmp_buf);
       break;
     case 1:
       /* generic/neon */
-      shuffle_generic(type_size, buffer_size, original, shuffled);
-      unshuffle_neon(type_size, buffer_size, shuffled, unshuffled);
+      bshuf_trans_bit_elem_scal(original, shuffled, num_elements, type_size, tmp_buf);
+      bitunshuffle_neon(shuffled, unshuffled,  num_elements, type_size, tmp_buf);
       break;
     case 2:
       /* neon/generic */
-      shuffle_neon(type_size, buffer_size, original, shuffled);
-      unshuffle_generic(type_size, buffer_size, shuffled, unshuffled);
+      bitshuffle_neon(original, shuffled, num_elements, type_size, tmp_buf);
+      bshuf_untrans_bit_elem_scal(shuffled, unshuffled, num_elements, type_size, tmp_buf);
       break;
     case 3:
       /* generic/generic */
-      shuffle_generic(type_size, buffer_size, original, shuffled);
-      unshuffle_generic(type_size, buffer_size, shuffled, unshuffled);
+      bshuf_trans_bit_elem_scal(original, shuffled, num_elements, type_size, tmp_buf);
+      bshuf_untrans_bit_elem_scal(shuffled, unshuffled, num_elements, type_size, tmp_buf);
       break;
     default:
       fprintf(stderr, "Invalid test type specified (%d).", test_type);
@@ -71,6 +76,7 @@ static int test_shuffle_roundtrip_neon(size_t type_size, size_t num_elements,
   }
   blosc_timestamp_t end;
   blosc_set_timestamp(&end);
+
   double elapsed = blosc_elapsed_secs(start, end);
   fprintf(stdout, "elapsed = %f\n", elapsed);
 
@@ -135,5 +141,5 @@ int main(int argc, char** argv) {
   }
 
   /* Run the test. */
-  return test_shuffle_roundtrip_neon(type_size, num_elements, buffer_align_size, test_type);
+  return test_bitshuffle_roundtrip_neon(type_size, num_elements, buffer_align_size, test_type);
 }
